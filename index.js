026369ab6f0a71a2862f6a91bc28ac790c001413
Enrik77 <enrik.mazai@outlook.com>
2024-06-18 14:16:18 +0300

    fixing playing

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import TrackPlayer from 'react-native-track-player';
+import playbackService from './service';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('YourAppName', () => App);
+TrackPlayer.registerPlaybackService(() => playbackService);
--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,99 +1,348 @@
-import { View, Text ,Dimensions,StyleSheet,SafeAreaView,TouchableOpacity} from 'react-native'
-import React from 'react'
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import { Image } from 'react-native';
-const { width, height} = Dimensions.get('window');
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  Animated,
+} from 'react-native';
+
+import TrackPlayer, {
+  Capability,
+  Event,
+  RepeatMode,
+  State,
+  usePlaybackState,
+  useProgress,
+  useTrackPlayerEvents,
+} from 'react-native-track-player';
+import Slider from '@react-native-community/slider';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import songs from '../model/data';
+
+const { width, height } = Dimensions.get('window');
+
+const setupPlayer = async () => {
+  try {
+    await TrackPlayer.setupPlayer();
+    await TrackPlayer.updateOptions({
+      capabilities: [
+        Capability.Play,
+        Capability.Pause,
+        Capability.SkipToNext,
+        Capability.SkipToPrevious,
+        Capability.Stop,
+      ],
+    });
+    await TrackPlayer.add(songs);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const togglePlayBack = async (playBackState) => {
+  const currentTrack = await TrackPlayer.getCurrentTrack();
+  if (currentTrack != null) {
+    if (playBackState === State.Paused || playBackState === State.Ready) {
+      await TrackPlayer.play();
+    } else {
+      await TrackPlayer.pause();
+    }
+  }
+};
 
 const MusicPlayer = () => {
-  return (
-    <SafeAreaView style= {style.container}>
-    <View style= {style.maincontainer}>
+  const playBackState = usePlaybackState();
+  const progress = useProgress();
+  const [songIndex, setsongIndex] = useState(0);
+  const [repeatMode, setRepeatMode] = useState('off');
+  const [trackTitle, setTrackTitle] = useState();
+  const [trackArtist, setTrackArtist] = useState();
+  const [trackArtwork, setTrackArtwork] = useState();
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const songSlider = useRef(null);
+
+  useTrackPlayerEvents([Event.PlaybackTrackChanged], async (event) => {
+    if (event.type === Event.PlaybackTrackChanged && event.nextTrack !== null) {
+      const track = await TrackPlayer.getTrack(event.nextTrack);
+      const { title, artwork, artist } = track;
+      setTrackTitle(title);
+      setTrackArtist(artist);
+      setTrackArtwork(artwork);
+    }
+  });
+
+  const repeatIcon = () => {
+    if (repeatMode === 'off') {
+      return 'repeat-off';
+    }
+
+    if (repeatMode === 'track') {
+      return 'repeat-once';
+    }
+
+    if (repeatMode === 'repeat') {
+      return 'repeat';
+    }
+  };
+
+  const changeRepeatMode = () => {
+    if (repeatMode === 'off') {
+      TrackPlayer.setRepeatMode(RepeatMode.Track);
+      setRepeatMode('track');
+    } else if (repeatMode === 'track') {
+      TrackPlayer.setRepeatMode(RepeatMode.Queue);
+      setRepeatMode('repeat');
+    } else if (repeatMode === 'repeat') {
+      TrackPlayer.setRepeatMode(RepeatMode.Off);
+      setRepeatMode('off');
+    }
+  };
+
+  const skipTo = async (trackId) => {
+    await TrackPlayer.skip(trackId);
+  };
+
+  useEffect(() => {
+    const initializePlayer = async () => {
+      await setupPlayer();
+
+      const listener = scrollX.addListener(({ value }) => {
+        const index = Math.round(value / width);
+        if (index !== songIndex) {
+          skipTo(index);
+          setsongIndex(index);
+        }
+      });
+
+      return () => {
+        scrollX.removeListener(listener);
+        TrackPlayer.destroy();
+      };
+    };
+
+    initializePlayer();
+  }, []);
+
+  const skipToNext = () => {
+    if (songIndex < songs.length - 1) {
+      songSlider.current.scrollToOffset({
+        offset: (songIndex + 1) * width,
+      });
+    }
+  };
+
+  const skipToPrevious = () => {
+    if (songIndex > 0) {
+      songSlider.current.scrollToOffset({
+        offset: (songIndex - 1) * width,
+      });
+    }
+  };
+
+  const renderSongs = ({ item }) => {
+    console.log('Rendering song:', item);
+    return (
+      <Animated.View style={style.mainWrapper}>
         <View style={[style.imageWrapper, style.elevation]}>
-            <Image source={require('../assets/img/img1.jpg')} 
-                style = {style.musicImage}
-            />
+          <Image
+            source={{ uri: String(item.artwork) }}
+            style={style.musicImage}
+          />
         </View>
-      
-    </View>
+      </Animated.View>
+    );
+  };
 
- <View style={style.bottomContainer}>
-    <View style={style.bottomIconWrapper}>
+  return (
+    <SafeAreaView style={style.container}>
+      {/* music player section */}
+      <View style={style.mainContainer}>
+        {/* Image */}
+        <Animated.FlatList
+          ref={songSlider}
+          renderItem={renderSongs}
+          data={songs}
+          keyExtractor={(item) => item.id}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={16}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: { x: scrollX },
+                },
+              },
+            ],
+            { useNativeDriver: true },
+          )}
+        />
 
-        <TouchableOpacity onPress={()=>{}}>
-        <Ionicons name='heart-outline' size={30} color="#888888" />
-        </TouchableOpacity>
+        {/* Title & Artist Name */}
+        <View>
+          <Text style={[style.songContent, style.songTitle]}>{trackTitle}</Text>
+          <Text style={[style.songContent, style.songArtist]}>{trackArtist}</Text>
+        </View>
 
-        <TouchableOpacity onPress={()=>{}}>
-        <Ionicons name='repeat' size={30} color="#888888" />
-        </TouchableOpacity>
+        {/* song slider */}
+        <View>
+          <Slider
+            style={style.progressBar}
+            value={progress.position}
+            minimumValue={0}
+            maximumValue={progress.duration}
+            thumbTintColor="#FFD369"
+            minimumTrackTintColor="#FFD369"
+            maximumTrackTintColor="#fff"
+            onSlidingComplete={async (value) => {
+              await TrackPlayer.seekTo(value);
+            }}
+          />
 
-        <TouchableOpacity onPress={()=>{}}>
-        <Ionicons name='share-outline' size={30} color="#888888" />
-        </TouchableOpacity>
+          {/* Progress Durations */}
+          <View style={style.progressLevelDuration}>
+            <Text style={style.progressLabelText}>
+              {new Date(progress.position * 1000).toISOString().substr(14, 5)}
+            </Text>
+            <Text style={style.progressLabelText}>
+              {new Date((progress.duration - progress.position) * 1000).toISOString().substr(14, 5)}
+            </Text>
+          </View>
+        </View>
 
-        <TouchableOpacity onPress={()=>{}}>
-        <Ionicons name='ellipsis-horizontal' size={30} color="#888888" />
-        </TouchableOpacity>
-    </View>
-    
- 
- </View>
- 
+        {/* music control */}
+        <View style={style.musicControlsContainer}>
+          <TouchableOpacity onPress={skipToPrevious}>
+            <Ionicons name="play-skip-back-outline" size={35} color="#FFD369" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => togglePlayBack(playBackState)}>
+            <Ionicons
+              name={playBackState === State.Playing ? 'pause-outline' : 'play-outline'}
+              size={75}
+              color="#FFD369"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={skipToNext}>
+            <Ionicons name="play-skip-forward-outline" size={35} color="#FFD369" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* bottom section */}
+      <View style={style.bottomSection}>
+        <View style={style.bottomIconContainer}>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={changeRepeatMode}>
+            <MaterialCommunityIcons
+              name={`${repeatIcon()}`}
+              size={30}
+              color={repeatMode !== 'off' ? '#FFD369' : '#888888'}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="share-outline" size={30} color="#888888" />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="ellipsis-horizontal" size={30} color="#888888" />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default MusicPlayer
+export default MusicPlayer;
 
 const style = StyleSheet.create({
-    container: { 
-        flex:1,
-        backgroundColor: '#222831',
-        
+  container: {
+    flex: 1,
+    backgroundColor: '#222831',
+  },
+  mainContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomSection: {
+    borderTopColor: '#393E46',
+    borderWidth: 1,
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 15,
+  },
+  bottomIconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  mainWrapper: {
+    width: width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 25,
+  },
+  imageWrapper: {
+    width: 300,
+    height: 340,
+    marginBottom: 25,
+  },
+  musicImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 15,
+  },
+  elevation: {
+    elevation: 5,
+    shadowColor: '#ccc',
+    shadowOffset: {
+      width: 5,
+      height: 5,
     },
-
-    maincontainer: {
-        flex:1,
-        alignContent: 'center',
-        justifyContent: 'center',
-    },
-
-    bottomContainer: {
-        width: width,
-        alignItems: 'center',
-        paddingVertical:15,
-        borderTopColor: '#393E46',
-        borderWidth:1,
-
-    },
-
-    bottomIconWrapper:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '88%',
-    },
-
-    imageWrapper:{
-        width: 300,
-        height: 340,
-        marginBottom: 25,
-
-    },
-    musicImage:{
-        width: '100%',
-        height:'100%',
-        borderRadius:15,
-    }
-    ,
-    elevation:{
-        elevation:5,
-        shadowColor:'#ccc',
-        shadowOffset:{
-            width:5,
-            height:5,
-            
-        },
-        shadowOpacity:0.5,
-        shadowRadius:3.86,
-    }
-  })
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+  },
+  songContent: {
+    textAlign: 'center',
+    color: '#EEEEEE',
+  },
+  songTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+  },
+  songArtist: {
+    fontSize: 16,
+    fontWeight: '300',
+  },
+  progressBar: {
+    width: 350,
+    height: 40,
+    marginTop: 25,
+    flexDirection: 'row',
+  },
+  progressLevelDuration: {
+    width: 340,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  progressLabelText: {
+    color: '#FFF',
+  },
+  musicControlsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 15,
+    width: '60%',
+  },
+});
